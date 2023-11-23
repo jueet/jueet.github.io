30a06ad045939e17356c1d7c300ee8e76ae00a34
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2023-11-23 11:45:10 -0700

    Update presentation.ts

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -16,7 +16,7 @@ const presentation: Presentation = {
   title: "Hi, I'm Edwin Olivo 👋",
   profile: "https://avatars.githubusercontent.com/u/46498127?v=4",
   description:
-    "A *software developer* and well-known *bookworm*. I love new technologies, and I'm constantly looking for something interesting to play around with. If you've got something, let me know!",
+    "Welcome to my portfolio! I'm a *seasoned software developer* ready to take you on a journey through my world of coding excellence. Dive into a showcase of innovative projects, impeccably crafted code, and a proven ability to transform ideas into impactful solutions. With expertise in *web development*, *software engineering*, and a knack for *problem-solving*, I invite you to discover the skills that set me apart. Your quest for a reliable and talented software developer ends right here.",
   socials: [
     {
       label: "Behance",
