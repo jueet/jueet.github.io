a6ec4f373c9318813b64f65ba7a7e03fa183c17f
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2024-02-26 16:20:03 -0700

    Refactor color handling in components

--- src/data/theme.ts
+++ src/data/theme.ts
@@ -2,7 +2,7 @@ import type { Theme } from "@/types";
 
 const theme: Theme = {
   colors: {
-    primary: "orange",
+    primary: "emerald",
     blur: {
       top: "orange",
       bottom: "violet",
